e80e0c2d752d3aaca3b3a8f9bc5fcf9cd7c80326
Miserable Coder <miserable.coder@gmail.com>
2015-03-03 19:58:35 +0300

    Added some comments

--- src/VKauto/Interfaces/WorkerInterface.php
+++ src/VKauto/Interfaces/WorkerInterface.php
@@ -4,7 +4,13 @@ namespace VKauto\Interfaces;
 
 interface WorkerInterface
 {
+	/**
+	 * Метод, отвечающий за начало работы воркера
+	 */
 	public function start();
 
+	/**
+	 * Останавлиает работу воркера
+	 */
 	public function stop();
 }
