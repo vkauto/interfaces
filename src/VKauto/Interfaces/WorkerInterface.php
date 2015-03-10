0c8c7580ce29cab1dfff2d4cdfb9fe54f95028af
Miserable Coder <miserable.coder@gmail.com>
2015-03-11 00:12:10 +0300

    Little update

--- src/VKauto/Interfaces/WorkerInterface.php
+++ src/VKauto/Interfaces/WorkerInterface.php
@@ -13,4 +13,10 @@ interface WorkerInterface
 	 * Останавлиает работу воркера
 	 */
 	public function stop();
+
+	/**
+	 * Здесь указывается, нужно ли передавать воркеру последним аргументом VKauto\Auth\Account
+	 * @return bool
+	 */
+	public function needsAccountClass();
 }
